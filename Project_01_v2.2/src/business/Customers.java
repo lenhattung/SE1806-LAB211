f74509ebc08c09a7ce7c862ca13b59bb8932df09
Tung Le <lenhattung@gmail.com>
2025-05-22 10:57:21 +0700

    ud
    ud

--- Project_01_v2.2/src/business/Customers.java
+++ Project_01_v2.2/src/business/Customers.java
@@ -6,31 +6,49 @@ package business;
 
 import java.util.HashSet;
 import models.Customer;
+import tools.Inputter;
 
 /**
  *
  * @author tungi
  */
-public class Customers extends HashSet<Customer> implements Workable<Customer, String>{
+public class Customers extends HashSet<Customer> implements Workable<Customer, String> {
 
+    public Customers() {
+    }
+    
+    public boolean isDupplicate(Customer t){
+        //return this.contains(t);
+        for (Customer c : this) {
+            if(c.getCustomerCode().equals(t.getCustomerCode())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
     @Override
     public void addNew(Customer t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.add(t);
     }
 
     @Override
     public void update(Customer t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void showAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("-------------------------------------------------------------");
+        System.out.format("%-6s | %-25s | %-11s | %-20s%n", "Code", "Customer Name", "Phone", "Email");
+        System.out.println("-------------------------------------------------------------");
+        for (Customer c : this) {
+            System.out.format("%-6s | %-25s | %-11s | %-20s%n", c.getCustomerCode(), c.getName(), c.getPhone(), c.getEmail());
+        }
+        System.out.println("-------------------------------------------------------------");
     }
 
     @Override
     public Customer searchById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return null;
     }
-    
 }
--- Project_01_v2.2/src/dispatcher/Main.java
+++ Project_01_v2.2/src/dispatcher/Main.java
@@ -4,6 +4,7 @@
  */
 package dispatcher;
 
+import business.Customers;
 import java.util.Scanner;
 import models.Customer;
 import tools.Inputter;
@@ -15,10 +16,46 @@ import models.SetMenu;
  * @author tungi
  */
 public class Main {
+
     public static void main(String[] args) {
-        Inputter inputter = new Inputter();
-        Customer c = inputter.inputCustomer();
-        Order o = inputter.inputOrder();
-        SetMenu s = inputter.inputSetMenu();
+        Inputter ip = new Inputter();
+        Customers customers = new Customers();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int testCase = 10;
+        do {
+            System.out.println("\n----------------------\n");
+            System.out.println("1. Register customers");
+            System.out.println("2. Update customer information");
+            System.out.println("3. Seach for customer information by name");
+            System.out.println("8. Display all customers");
+            System.out.println("10. Exit");
+            System.out.print("Enter Test Case No. : ");
+            testCase = Integer.parseInt(scanner.nextLine());
+            switch (testCase) {
+                case 1:
+                    int option = 0;
+                    do {
+                        Customer c = ip.inputCustomer();
+                        if (!customers.isDupplicate(c)) {
+                            customers.addNew(c);
+                        } else {
+                            System.out.println("Error: Customer Aldready Exists!");
+                        }
+
+                        System.out.println("1. Continue entering new customers");
+                        System.out.println("2. Return to the main menu");
+                        System.out.println("Entor your option: ");
+                        option = Integer.parseInt(scanner.nextLine());
+                    } while (option != 2);
+                    break;
+                case 8:
+                    customers.showAll();
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (testCase != 10);
     }
 }
